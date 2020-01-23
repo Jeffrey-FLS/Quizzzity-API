48e9f23f150337b50bc4de49d831437de22f314d
Jeffrey Paulino <jp7kode@gmail.com>
2020-01-23 09:07:17 -0500

    creating questions

--- app/controllers/api/v1/quizzes_controller.rb
+++ app/controllers/api/v1/quizzes_controller.rb
@@ -7,4 +7,45 @@ class Api::V1::QuizzesController < ApplicationController
     render json: @quizzes
   end
 
+
+  #def review
+  #  review = Review.create({
+  #                             content: params[:content],
+  #                             user: session_user,
+  #                             movie_id: params[:id].to_i,
+  #                             stars: params[:stars]
+  #                         })
+  #
+  #  if review.save
+  #    render json: review, status: :ok
+  #  else
+  #    render json: {errors: review.errors.full_messages[0]}
+  #  end
+  #end
+
+  def create
+    #quiz = Quiz.new(
+    #    title: params[:]
+    #)
+    #
+
+    render json:params
+  end
+
+
+  #def create
+  #  user = User.new(
+  #      username: params[:username],
+  #      password: params[:password],
+  #      )
+  #
+  #  if user.save
+  #    token = encode_token(user.id)
+  #    render json: {user: user, token: token}
+  #  else
+  #    render json: {errors: user.errors.full_messages}
+  #  end
+  #end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :quizzes
       resources :users
+
+      post "/create-quiz", to: "quizzes#create"
     end
   end
 
